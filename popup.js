f2a00192c6bb4ea0e853a67d8ee3d6621464d391
Abhinav Singh <abhinav.s0612@gmail.com>
2024-04-14 17:58:32 +0530

    refactor: remove unused files

--- popup.js
+++ popup.js
@@ -1,9 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   var storageData = null;
   const dropdown = document.getElementById("keysDropdown");
+
   var currentSelectedValue = null;
   var currentStorageType = "localStorage"; // default to localStorage
-  
+
   function updateDropdown() {
     dropdown.innerHTML = ""; // clear the dropdown
     const storageKeys = Object.keys(storageData);
@@ -16,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
       const defaultOptionElement = document.createElement("option");
       defaultOptionElement.text = "Select a key";
       dropdown.add(defaultOptionElement);
-      storageKeys.forEach(key => {
+      storageKeys.forEach((key) => {
         const optionElement = document.createElement("option");
         optionElement.value = key;
         optionElement.text = key;
@@ -26,55 +27,77 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   function updateStorageData() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { action: "getStorageData", storageType: currentStorageType }, function(response) {
-        storageData = response.storageData;
-        updateDropdown();
-      });
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { action: "getStorageData", storageType: currentStorageType },
+        function (response) {
+          storageData = response.storageData;
+          updateDropdown();
+        }
+      );
     });
   }
 
   updateStorageData();
 
-  dropdown.addEventListener("change", function() {
+  dropdown.addEventListener("change", function () {
     currentSelectedValue = dropdown.value;
-    console.log(`Selected value: ${currentSelectedValue}`);
+    console.log(
+      `Selected Value: ${currentSelectedValue}`,
+      typeof storageData[currentSelectedValue],
+      storageData[currentSelectedValue]
+    );
+    if (
+      typeof storageData[currentSelectedValue] === "object" ||
+      (typeof storageData[currentSelectedValue] === "string" &&
+        isJsonified(storageData[currentSelectedValue]))
+    ) {
+      console.log("its an object");
+    }
   });
 
   const localStorageRadio = document.getElementById("localStorageRadio");
   const sessionStorageRadio = document.getElementById("sessionStorageRadio");
 
-  localStorageRadio.addEventListener("change", function() {
+  localStorageRadio.addEventListener("change", function () {
     currentStorageType = "localStorage";
     updateStorageData();
   });
 
-  sessionStorageRadio.addEventListener("change", function() {
+  sessionStorageRadio.addEventListener("change", function () {
     currentStorageType = "sessionStorage";
     updateStorageData();
   });
 
-  var grabValueButton = document.getElementById('grabValueButton');
-  grabValueButton.addEventListener('click', function() {
-    const showValueCheckbox = document.getElementById('showValueCheckbox');
-    const copyToClipboardCheckbox = document.getElementById('copyToClipboardCheckbox');
-    
-    if (currentSelectedValue && storageData && storageData[currentSelectedValue]) {
+  var grabValueButton = document.getElementById("grabValueButton");
+  grabValueButton.addEventListener("click", function () {
+    const showValueCheckbox = document.getElementById("showValueCheckbox");
+    const copyToClipboardCheckbox = document.getElementById(
+      "copyToClipboardCheckbox"
+    );
+
+    if (
+      currentSelectedValue &&
+      storageData &&
+      storageData[currentSelectedValue]
+    ) {
       const selectedValue = storageData[currentSelectedValue];
       if (showValueCheckbox.checked) {
         copyContent(selectedValue, true);
       }
-      
+
       if (copyToClipboardCheckbox.checked) {
         copyContent(selectedValue);
       }
     }
   });
-  
-  function copyContent(text, showAlert=false) {
+
+  function copyContent(text, showAlert = false) {
     try {
       text = text.toString();
-      navigator.clipboard.writeText(text)
+      navigator.clipboard
+        .writeText(text)
         .then(() => {
           if (showAlert) {
             alert(text);
@@ -89,9 +112,18 @@ document.addEventListener('DOMContentLoaded', function() {
         .catch((err) => {
           alert(err);
         });
-      console.log('Content copied to clipboard');
+      console.log("Content copied to clipboard");
     } catch (err) {
-      window.alert('Some error occurred!');
+      window.alert("Some error occurred!");
     }
   }
 });
+
+const isJsonified = (input) => {
+  try {
+    JSON.parse(input);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
